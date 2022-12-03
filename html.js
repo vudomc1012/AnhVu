0a9536f218600139d8f53848f9634122e3726084
Đỗ Anh Vũ <71638421+vudomc1012@users.noreply.github.com>
2022-12-03 18:17:58 +0700

    Update View
    nothing

---
+++ html.js
@@ -0,0 +1,42 @@
+
+let christmas = {
+    delay: null,
+    delete: function(){
+        document.body.querySelectorAll('.christmas-lights').forEach(function(ul){
+            ul.remove();
+        });
+    },
+    create: function(){
+        let v = window.innerHeight / 60 + 2,
+            h = window.innerWidth / 60 + 2,
+            data = {
+                'top': h,
+                'right': v,
+                'bottom': h,
+                'left': v
+            },
+            ul = c = null;
+        for (let position in data) {
+            c = data[position];
+            ul = document.createElement('ul');
+            ul.className = 'christmas-lights';
+            ul.dataset.position = position;
+            for (let i = 0; i <= c; i++) {
+                ul.append(document.createElement('li'));
+            }
+            document.body.append(ul);
+        }
+    }
+}
+ 
+document.addEventListener('DOMContentLoaded', function(){
+    christmas.create();
+});
+ 
+window.addEventListener('resize', function(e) {
+    clearTimeout(christmas.delay);
+    christmas.delay = setTimeout(function(){
+        christmas.delete();
+        christmas.create();
+    }, 100)
+});
